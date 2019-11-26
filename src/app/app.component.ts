dcf0afe0a0a318ac2b5d93b1e5cc73542c88f3cb
Briana Connolly <bconnolly555@gmail.com>
2019-11-26 00:35:41 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <!-- header -->
+    <app-header></app-header>
+    <!-- routes will be rendered here -->
+    <router-outlet></router-outlet>
+  `,
+  styles: []
 })
 export class AppComponent {
   title = 'techware-frontend';
